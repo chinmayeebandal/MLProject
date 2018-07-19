dfdbc643a88718172880ee9b9ae68b9724961e67
chinmayeebandal <cbandal@buffalo.edu>
2018-07-19 01:36:36 -0400

    Update StockRegre.py

--- StockRegre.py
+++ StockRegre.py
@@ -1,26 +1,29 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
+
 """
 Created on Wed Jul 18 14:32:10 2018
 
-Machine learning project which predicts the cost of stock prices 30 days in advance by applying
+Machine learning project which predicts the cost of stock prices 1 day in advance by applying
 Linear Regression on current financial data
 
 @author: cbandal
 """
 
-import quandl
-quandl.ApiConfig.api_key = "Enter_your_API_key"
+import quandl, math
+quandl.ApiConfig.api_key = "Enter_your_APIKey"
 
 import pandas as pd
 import matplotlib.pyplot as plt
+from matplotlib import style
+import datetime as datetime
 
 from sklearn.linear_model import LinearRegression
-from sklearn import preprocessing, cross_validation
+from sklearn import preprocessing
+from sklearn.model_selection import train_test_split
 
 import numpy as np
-#import tensorflow as tf
 
+
+style.use('ggplot')
 
 ticker = input("Enter ticker symbol: ")
 symbol = 'WIKI/'+ticker;
@@ -31,13 +34,22 @@ df = data.to_pandas()
 df.to_csv('~/Documents/SummerProjects/MLProject/'+ticker+'.csv')
 
 def load_stock_data():
-    return pd.read_csv('~/Documents/SummerProjects/MLProject/AAPL.csv')
-    
-#filter out Adj close
-df = df[['Adj. Close']]
+    return pd.read_csv('~/Documents/SummerProjects/MLProject/'+ticker+'.csv', header=0, index_col='Date', parse_dates=True)
+
+#filter out columns
+df = load_stock_data()
+df = df.filter(items=['Date', 'Adj. Open', 'Adj. High', 'Adj. Low', 'Adj. Close', 'Adj. Volume'])
+
+
 print(df.tail()) #print 5 recent closing costs
 
-forecast_out = int(30)
+df['HL_PCT'] = (df['Adj. High'] - df['Adj. Low']) / df['Adj. Close'] * 100.0
+df['PCT_change'] = (df['Adj. Close'] - df['Adj. Open']) / df['Adj. Open'] * 100.0
+df = df[['Adj. Close', 'HL_PCT', 'PCT_change', 'Adj. Volume']]
+
+df.fillna(value=-99999, inplace=True)
+forecast_out = int(math.ceil(0.01 * len(df)))
+
 df['Prediction'] = df[['Adj. Close']].shift(-forecast_out)
 
 #scale the data
@@ -48,13 +60,14 @@ X = preprocessing.scale(X)
 close_forecast = X[-forecast_out:]
 X = X[:-forecast_out]
 
+df.dropna(inplace=True)
+
 y = np.array(df['Prediction'])
-y = y[:-forecast_out]
 
 #generate test and training data
-X_train, X_test, y_train, y_test = cross_validation.train_test_split(X, y, test_size = 0.2)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2)
 
-linR = LinearRegression()
+linR = LinearRegression(n_jobs=-1)
 
 #train data
 linR.fit(X_train, y_train)
@@ -66,9 +79,23 @@ print("testing: ", testing)
 #predict 30 days into future
 prediction = linR.predict(close_forecast)
 print(prediction)
+df['Forecast'] = np.nan
 
-#plot the graph
-plt.ylabel("Cost")
-plt.xlabel("No of days from today")
-plt.plot(prediction)
+last_date = df.iloc[-1].name
+last_unix = last_date.timestamp()
+one_day = 86400
+next_unix = last_unix + one_day
+
+for i in prediction:
+    next_date = datetime.datetime.fromtimestamp(next_unix)
+    next_unix += 86400
+    df.loc[next_date] = [np.nan for _ in range(len(df.columns)-1)]+[i]
+
+
+df['Adj. Close'].plot(figsize=[10,9])
+df['Forecast'].plot(figsize=[10,9])
+
+plt.legend(loc=4)
+plt.xlabel('Date')
+plt.ylabel('Price')
 plt.show()
